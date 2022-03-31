5b01fd79a1cc50d16288f3fd7af275c6c4e2593f
WhatisaRyker <rykernboyko@gmail.com>
2022-03-31 07:40:45 -0400

    oh boy here we go

--- overlays-plus/Plugin.cs
+++ overlays-plus/Plugin.cs
@@ -2,12 +2,15 @@
 using IPA.Config;
 using IPA.Config.Stores;
 using System;
+using System.Net;
 using System.Collections;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using IPALogger = IPA.Logging.Logger;
+using System.Text;
 
 namespace overlays_plus
 {
@@ -16,6 +19,7 @@ namespace overlays_plus
     {
         internal static Plugin Instance { get; private set; }
         internal static IPALogger Log { get; private set; }
+        static HttpListener _httpListener = new HttpListener();
 
         [Init]
         /// <summary>
@@ -28,6 +32,20 @@ namespace overlays_plus
             Instance = this;
             Log = logger;
             Log.Info("overlays-plus initialized.");
+        }
+
+        static void ResponseThread()
+        {
+            while (true)
+            {
+                HttpListenerContext context = _httpListener.GetContext(); // get a context
+                                                                          // Now, you'll find the request URL in context.Request.Url
+                byte[] _responseArray = Encoding.UTF8.GetBytes("<html><head><title>Localhost server -- port 5000</title></head>" + "<body>Welcome to the <strong>Localhost server</strong> -- <em>port 5000!</em></body></html>"); // get the bytes to response
+                context.Response.OutputStream.Write(_responseArray, 0, _responseArray.Length); // write bytes to the output stream
+                context.Response.KeepAlive = false; // set the KeepAlive bool to false
+                context.Response.Close(); // close the connection
+                Log.Info("Respone given to a request.");
+            }
         }
 
         #region BSIPA Config
@@ -47,7 +65,18 @@ namespace overlays_plus
         {
             Log.Debug("OnApplicationStart");
             new GameObject("overlays_plusController").AddComponent<overlays_plusController>();
+            Log.Info("web server starting...");
+            _httpListener.Prefixes.Add("http://localhost:5000/"); // add prefix "http://localhost:5000/"
+            _httpListener.Start(); // start server (Run application as Administrator!)
+            Log.Info("Server started.");
+            Thread _responseThread = new Thread(ResponseThread);
+            _responseThread.Start(); // start the response thread
 
+        }
+
+        public void Update() 
+        {
+            
         }
 
         [OnExit]
